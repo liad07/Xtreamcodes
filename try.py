fa4abf44718688f6364ba96ec2a56a38661ef08d
liadk07 <91607695+liad07@users.noreply.github.com>
2023-02-28 22:07:27 +0200

    Add files via upload

---
+++ try.py
@@ -0,0 +1,21 @@
+m3u_file = 'tv_channels_SabSmulik60.m3u'
+
+with open(m3u_file) as f:
+    lines = f.readlines()
+
+channels = []
+name = ''
+for line in lines:
+    line = line.strip()
+    if line.startswith('#EXTINF:'):
+        name = line.split(',')[1]
+    elif line.startswith('http'):
+        url = line
+        channels.append((name, url))
+
+for name, url in channels:
+    file_name = name.lower().replace(' ', '_') + '.m3u'
+    with open(file_name, 'w') as f:
+        f.write('#EXTM3U\n')
+        f.write(f'#EXTINF:-1,{name}\n')
+        f.write(f'{url}\n')
